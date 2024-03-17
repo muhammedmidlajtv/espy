5626acb81987bbb27ac295eccabad9a816f3d749
unknown <muhammedmidlajtv@gmail.com>
2024-03-17 23:40:10 +0530

    splash screen time updated

--- lib/screen/splash.dart
+++ lib/screen/splash.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   _navigatetohome() async {
-    await Future.delayed(Duration(milliseconds: 2000), () {});
+    await Future.delayed(Duration(milliseconds: 1500), () {});
     Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => ScreenHome()));
   }
